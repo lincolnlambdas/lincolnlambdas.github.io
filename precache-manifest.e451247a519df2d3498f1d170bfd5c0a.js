820ed3b4d0fad7c90b14af4fe95c26f4ab0bdf23
lincolnlambdas <unl.lphie@gmail.com>
2019-08-17 14:50:56 -0500

    Updates

---
+++ precache-manifest.e451247a519df2d3498f1d170bfd5c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a375459988ca91112b3a9d10f204ee01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15d007a70e0908a41cda",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "12cc09eeecdabbd831d0",
+    "url": "/static/js/2.d22242ab.chunk.js"
+  },
+  {
+    "revision": "15d007a70e0908a41cda",
+    "url": "/static/js/main.0fe03f9f.chunk.js"
+  },
+  {
+    "revision": "da14ba5949e2e8a280cd",
+    "url": "/static/js/runtime~main.453e3608.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
